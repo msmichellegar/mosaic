98600feabaf866a51d133c1c38d95f4383f65c9e
msmichellegar <msmichellegar@gmail.com>
2016-05-30 14:53:07 +1000

    resizes images on upload

--- js/client.js
+++ js/client.js
@@ -41,8 +41,8 @@ function getImagePath(file) {
   var reader = new FileReader();
 
   reader.onload = function(event) {
-    createImageElement(event.target.result)
-  }
+    createImageElement(event.target.result);
+  };
 
   reader.readAsDataURL(file);
 
@@ -51,16 +51,51 @@ function getImagePath(file) {
 // creates image element
 function createImageElement(imageSrc) {
   var imageElement = new Image();
+  var scaledImage;
 
   imageElement.src = imageSrc;
 
+  scaledImage = scaleImage(imageElement);
+
   imageElement.onload = function () {
-    sliceImage(imageElement);
+    sliceImage(scaledImage);
+  };
+}
+
+function scaleImage(image) {
+  var canvas = document.createElement("canvas");
+  var ctx = canvas.getContext("2d");
+
+  var MAX_WIDTH = 700;
+  var MAX_HEIGHT = 600;
+  var width = image.width;
+  var height = image.height;
+
+  if (width > height) {
+    if (width > MAX_WIDTH) {
+      height *= MAX_WIDTH / width;
+      width = MAX_WIDTH;
+    }
+  } else {
+    if (height > MAX_HEIGHT) {
+      width *= MAX_HEIGHT / height;
+      height = MAX_HEIGHT;
+    }
   }
+
+  canvas.width = width;
+  canvas.height = height;
+  ctx.drawImage(image, 0, 0, width, height);
+
+  return ctx;
+
 }
 
 // slices images into tiles
-function sliceImage(image) {
+function sliceImage(context) {
+  var image = new Image();
+  image.src = context.canvas.toDataURL();
+
   var originalCanvas = document.getElementById("canvas");
 
   var canvas = document.createElement("canvas");
@@ -77,9 +112,9 @@ function sliceImage(image) {
 	canvas.width = TILE_WIDTH;
 	canvas.height = TILE_HEIGHT;
 
-  for (var column=0; column < numberOfColumns; column++) {
+  for (var row=0; row < numberOfRows; row++) {
 
-    for (var row=0; row < numberOfRows; row++) {
+    for (var column=0; column < numberOfColumns; column++) {
       var tile;
 
       context.drawImage(image, column * TILE_WIDTH, row * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, 0, 0, TILE_WIDTH, TILE_HEIGHT);
@@ -107,12 +142,13 @@ function buildMosaic(slicedImages) {
   for (var i=0; i < slicedImages.length; i++) {
     var averageColour = getAverageColour(slicedImages[i]);
 
-    displayTile(averageColour, slicedImages[i])
+    displayTile(averageColour, slicedImages[i]);
 
   }
 
 }
 
+// gets average colour of the tile
 function getAverageColour(slicedImage) {
   var canvas = document.createElement("canvas");
   var context = canvas.getContext("2d");
@@ -153,6 +189,7 @@ function getAverageColour(slicedImage) {
 
 }
 
+// converts rgb value to hex code
 function convertRgbToHex(redValue, greenValue, blueValue, slicedImage) {
   var redHex = redValue.toString(16);
   var greenHex = greenValue.toString(16);
@@ -173,17 +210,13 @@ function convertRgbToHex(redValue, greenValue, blueValue, slicedImage) {
 
 }
 
-// fetches colour tile from server
+// draws colour tile to canvas
 function displayTile(colour, slicedImage) {
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
 
   var tile = new Image();
   tile.src = "/color/" + colour;
-
-  console.log(context)
-
-  // console.log(tile, slicedImage.x, slicedImage.y, TILE_WIDTH, TILE_HEIGHT)
 
   tile.onload = function() {
 
